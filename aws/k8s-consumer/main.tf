d1adb86117987f100ca8f3cd55f3db4730783f37
Zander Nelson <zander.nelson@ibm.com>
2026-02-04 13:18:34 -0500

    fix: restore tfe_outputs and locals to k8s-consumer

--- aws/k8s-consumer/main.tf
+++ aws/k8s-consumer/main.tf
@@ -2,3 +2,15 @@
 # This workspace provisions resources that consume secrets from Vault
 # using the Kubernetes authentication method.
 
+# Consume network outputs from vault-network-aws-sandbox workspace
+data "tfe_outputs" "network" {
+  organization = "zander-nelson-demo"
+  workspace    = "vault-network-aws-sandbox"
+}
+
+locals {
+  vpc_id             = data.tfe_outputs.network.values.vpc_id
+  public_subnet_ids  = data.tfe_outputs.network.values.public_subnet_ids
+  private_subnet_ids = data.tfe_outputs.network.values.private_subnet_ids
+  name_prefix        = data.tfe_outputs.network.values.name_prefix
+}
